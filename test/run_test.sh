e3ed0abd0a7576227cd1ee0595cf521bf5745bde
huafei1137 <huafei@wustl.edu>
2023-08-06 22:05:35 -0700

    test

--- test/run_test.sh
+++ test/run_test.sh
@@ -5,12 +5,13 @@ echo "" > output_summary.txt
 # Using find command to get all files and iterate over them
 for file in $(find "$dir" -type f -name "*.qasm")
 do
-  echo "Processing $file"
-  echo "Processing $file :" >> output_summary.txt
-  ./../build/nwq_qasm -q $file | grep "time is" >> output_summary.txt
-  echo "with limited coupling graph" >> output_summary.txt
-  ./../build/nwq_qasm -q $file -limited | grep "time is" >> output_summary.txt
+  echo "===== Processing $file: ====="
+  # echo "Processing $file :" >> output_summary.txt
+  ./../build/qasmtrans -i $file -c ../data/devices/ibmq_toronto.json -o output.qasm -v 1 -limited >> output_summary.txt
+  echo " ===== end ======="
+  # echo "with limited coupling graph" >> output_summary.txt
+  # ./../build/nwq_qasm -q $file -limited | grep "time is" >> output_summary.txt
   # Add your processing commands here
 done
 
-echo "Finish all test files, dump file has been stored in data/output_qasm_file"
+echo "Finish all test files, result store in the output_summary.txt"
